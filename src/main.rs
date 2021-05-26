78d1868d87e9b84e989a31807743e9b31514375f
Jakub Kozłowski <kubukoz@gmail.com>
2021-05-26 23:58:42 +0200

    Complete guessing game

--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,30 @@ fn main() {
     let secret_number = thread_rng().gen_range(1..101);
 
     println!("The secret number is: {}", secret_number);
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
 
     loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(v) => v,
+            Err(_) => continue,
+        };
 
         println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_number) {
             std::cmp::Ordering::Less => println!("Too small!"),
             std::cmp::Ordering::Greater => println!("Too big!"),
-            std::cmp::Ordering::Equal => println!("You win!"),
+            std::cmp::Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         }
     }
 }
