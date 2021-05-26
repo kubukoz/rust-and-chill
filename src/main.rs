cbe33c273e950058fc91b2c97826b9c826f39dfc
Jakub Kozłowski <kubukoz@gmail.com>
2021-05-26 23:21:55 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,27 @@
+use std::io;
+
+use rand::{thread_rng, Rng};
+
 fn main() {
-    let x = fun() + fun() + fun();
+    println!("Guess the number!");
+    let secret_number = thread_rng().gen_range(1..101);
 
-    println!("Hello, world! x is {}", x);
-}
+    println!("The secret number is: {}", secret_number);
+    println!("Please input your guess.");
 
-fn fun() -> i8 {
-    return 42;
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+    println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        std::cmp::Ordering::Less => println!("Too small!"),
+        std::cmp::Ordering::Greater => println!("Too big!"),
+        std::cmp::Ordering::Equal => println!("You win!"),
+    }
 }
