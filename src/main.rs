40b079155ea6694ecc35633853e266a19676fc11
Jakub Kozłowski <kubukoz@gmail.com>
2021-06-08 22:34:50 +0200

    some git stuff

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,78 @@
-use std::io;
+use std::{collections::LinkedList, env::args, fs::remove_file, path::Path};
 
-use rand::{thread_rng, Rng};
+use git2::{IndexConflict, Repository};
 
 fn main() {
-    /// Just for fn
-    fn parse_i32(guess: String) -> Option<i32> {
-        return guess.trim().parse::<i32>().ok();
-    }
+    let repo_path = args()
+        .nth(1)
+        .expect("The path to the directory wasn't provided!");
 
-    println!("Guess the number!");
-    let secret_number = thread_rng().gen_range(1..101);
+    let repo =
+        Repository::open(&repo_path).expect(&format!("Couldn't open repository at {}", repo_path));
 
-    println!("The secret number is: {}", secret_number);
+    let index = repo.index().expect("Couldn't get index");
 
-    loop {
-        println!("Please input your guess.");
+    index
+        .conflicts()
+        .unwrap()
+        .map(Result::unwrap)
+        .for_each(|conflict| {
+            let o = &conflict.our;
+            // let chuj = &o.map(|p| p.path);
 
-        let mut guess = String::new();
+            println!("{:?}", &conflict.their.map(|p| p.path));
 
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
+            let our = o.expect("Conflicting file was removed on the left-hand side!");
 
-        let guess = match parse_i32(guess) {
-            Some(value) => value,
-            None => continue,
-        };
+            let file_path = String::from_utf8(our.path).unwrap();
 
-        println!("You guessed: {}", guess);
+            let should_delete = conflict.their.is_none();
 
-        match guess.cmp(&secret_number) {
-            std::cmp::Ordering::Less => println!("Too small!"),
-            std::cmp::Ordering::Greater => println!("Too big!"),
-            std::cmp::Ordering::Equal => {
-                println!("You win!");
-                break;
+            if should_delete {
+                let file_path = Path::new(&file_path);
+
+                println!("Will REMOVE file {:?}", file_path);
+
+                // let path_to_remove = Path::new(&repo_path).join(Path::new(&file_path));
+                // remove_file(path_to_remove).unwrap();
+
+                // repo.index().unwrap().add_path(&file_path).unwrap();
+                repo.index().unwrap().remove_path(&file_path).unwrap()
+                // repo.index()
+                //     .unwrap()
+                //     .remove_path(Path::new(&(repo_path.to_owned() + "/" + &file_path)))
+                //     .unwrap();
+                // index.remove_path(Path::new(&file_path));
+            } else {
+                println!("Will take THEIR version of {}", file_path)
+            };
+        });
+    /*
+    repo.statuses(None)
+        .expect("Couldn't list file statuses")
+        .iter()
+        .for_each(|entry| {
+            if entry.status().is_conflicted()
+                && entry
+                    .path()
+                    .unwrap_or_else(|| "")
+                    .ends_with("EngineMocking.scala")
+            {
+                println!("{:#?}", entry.status());
+                // println!("huh? {}", entry);
+                // repo.checkout_index(
+                //     Some(&mut index),
+                //     Some(
+                //         CheckoutBuilder::new()
+                //             .path(entry.path().unwrap())
+                //             .use_theirs(true)
+                //             .allow_conflicts(true),
+                //     ),
+                // )
+                // .expect(&format!(
+                //     "Couldn't check in conflicting file {}",
+                //     entry.path().expect("<none>")
+                // ));
             }
-        }
-    }
+        }); */
 }
